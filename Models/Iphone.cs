fb32e02a021d6632eaef418ea42fb28a6b980efc
Alan Santos <alanfds304@gmail.com>
2024-01-14 23:40:36 -0300

    Desafio Feitogit add Models/Smartphone.cs!

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -2,7 +2,42 @@ namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
     public class Iphone
+
     {
+      public string Numero{get;set;}
+
+
+
+      public Iphone (string numero)
+
+      {
+
+      Numero = numero;
+
+      }
+
+      public void Ligar()
+      {
+
+      Console.WriteLine("Ligando....");
+        
+      }
+
+      public void ReceberLigacao()
+
+      {
+
+    Console.WriteLine("Recebendo Ligacao.....");
+
+
+      }
+
+
+
+      
+
+
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
     }
 }
--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,41 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+   
+    public class Nokia:Smartphone
+
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+     public string Numero {get;set;}
+       
+     
+     public Nokia (string numero, string modelo, string imei , int memoria): base(numero, modelo ,imei, memoria)
+     
+
+     {
+
+      
+
+     }
+
+     public override void InstalarAplicativo(string nome)
+     {
+            // Implementação do método InstalarAplicativo para a classe Iphone
+            Console.WriteLine($"Instalando o aplicativo: {nome} no nokia.");
+     }
+
+     public void Ligar()
+     {
+        Console.WriteLine("Ligando.....");
+
+     }
+
+     public void ReceberLigacao()
+     {
+        Console.WriteLine("Recebendo Ligacao.....");
+       
+     }
+
+     // TODO: Sobrescrever o método "InstalarAplicativo"
+     
     }
 }
